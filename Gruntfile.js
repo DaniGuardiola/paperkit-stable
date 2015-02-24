2c74c8bfd2b3f02bc325c74d0760625bc8650d35
Dani Guardiola <dani.guardiola@datatrends.es>
2015-02-24 11:32:38 +0100

    New release

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Configuration
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      // Releaser
+      release: {
+        options: {
+          file: 'bower.json', //default: package.json
+          npm: false, //default: true
+          github: {
+            repo: 'daniguardiola/paperkit-stable', //put your user/repo here
+            usernameVar: 'GITHUB_USERNAME', //ENVIRONMENT VARIABLE that contains Github username
+            passwordVar: 'GITHUB_PASSWORD' //ENVIRONMENT VARIABLE that contains Github password
+          }
+        }
+      },
+
+      // Shell tasks
+      exec: {
+        stageAll: {
+          command: 'git add . --all && git reset -- bower.json && git commit -m "New release" --allow-empty && git push -q'
+        }
+      }
+  });
+
+  // Loading NPM tasks
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-exec');
+
+  // Tasks
+  grunt.registerTask('default', ['exec:stageAll', 'release']);
+};
+
